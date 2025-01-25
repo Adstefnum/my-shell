7d6ea925a518ec79f330c079b29b47ffd711745e
Stephen Adesina <bauerstevenson@gmail.com>
2025-01-25 05:48:16 +0200

    trying to see if the windows fix was the issue

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -10,7 +10,7 @@
 std::string exec(const char* cmd) {
     std::array<char, 128> buffer;
     std::string result;
-    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    std::shared_ptr<FILE> pipe(_popen(cmd, "r"), _pclose);
     if (!pipe) throw std::runtime_error("popen() failed!");
     while (!feof(pipe.get())) {
         if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
