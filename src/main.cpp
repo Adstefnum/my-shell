c9ee66d60b3988a17c17bc70c87490ef3132e363
Stephen Adesina <bauerstevenson@gmail.com>
2025-01-25 16:22:58 +0200

    not running the program with the full path

--- src/main.cpp
+++ src/main.cpp
@@ -29,11 +29,8 @@ int main() {
       });
     } else {
       std::string exe_path = Helpers::exec(("which " + args[0]).c_str());
-      std::cout << "exe_path: " << exe_path << std::endl;
-      std::cout << "args[0]: " << args[0] << std::endl;
-      std::cout <<  "args[1]: " << args[1] << std::endl;
       if (exe_path != "") {
-        std::string command = exe_path + " " + args[1];
+        std::string command = args[0] + " " + args[1];
         Helpers::exec(command.c_str());
       } else {
         std::cout << args[0] << ": command not found" << std::endl;
